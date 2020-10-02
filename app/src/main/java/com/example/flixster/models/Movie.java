e1de5ed9774626a1e72100088a9abfbdc7495bab
Joan <jaoanan@zagmail.gonzaga.edu>
2020-10-02 10:53:13 -0700

    Flixster 2

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,22 +3,30 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String posterPath;
     String title;
     String overview;
     String backdropPath;
+    int movieId;
+    double rating;
 
+    // get rating, parse it first, field, add a getter
+    //Emoty constructor needed by the Parceler library
+    public Movie(){ }
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         backdropPath = jsonObject.getString("backdrop_path");
-
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -39,6 +47,14 @@ public class Movie {
 
     public String getBackDropPath(){
         return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 
     public String getOverview() {
